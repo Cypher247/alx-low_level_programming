cee26a0f5ec68565f99c9202863ff3d18fa0b53e
Timilehin <timilehinolaniyan1@gmail.com>
2023-08-18 15:45:25 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -prog checks for capital characters
+ * @c: Variable text
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && C <= '2')
+}
+return (1);
+}
+else
+{
+return (0);
+}
+}
